8417bbc13f75d92ddd10455498cbb800f9f71334
SayKnowToCode <ninadmaadhavi13@gmail.com>
2025-02-09 01:36:23 +0530

    Extensions Deleted

--- extension/popup.js
+++
@@ -1,9 +0,0 @@
-document.getElementById('startRecord').addEventListener('click', async () => {
-  const [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
-  chrome.tabs.sendMessage(tab.id, { action: "START_RECORDING" });
-});
-
-document.getElementById('stopRecord').addEventListener('click', async () => {
-  const [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
-  chrome.tabs.sendMessage(tab.id, { action: "STOP_RECORDING" });
-});
